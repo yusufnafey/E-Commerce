37387cf4f4fb44c59d497c7e454eb2484ed889c0
Yusuf Nafey <yusufnafey@yahoo.com>
2019-03-14 23:45:35 -0700

    navbar created

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import "./App.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 import logo from "./logo.svg";
@@ -6,12 +6,9 @@ import logo from "./logo.svg";
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col-6">column one</div>
-          <div className="col-6">column two</div>
-        </div>
-      </div>
+      <Fragment>
+        <h3>App.js</h3>
+      </Fragment>
     );
   }
 }
---
+++ src/Components/Navbar.js
@@ -0,0 +1,13 @@
+import React, { Component, Fragment } from "react";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h3>Navbar</h3>
+      </Fragment>
+    );
+  }
+}
+
+export default Navbar;
