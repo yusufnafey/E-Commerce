3f9d7796dd013cf8e151746bdef3c35d78c18190
Yusuf Nafey <yusufnafey@yahoo.com>
2019-03-17 23:02:39 -0700

    added nav items

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,12 +5,24 @@ import logo from "../logo.svg";
 export default class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-sm navbar-dark px-sm-5">
+      <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
         {/* https://www.iconfinder.com/icons/1243689/call_phone_icon
 Creative Commons (Attribution 3.0 Unported);
 https://www.iconfinder.com/Makoto_msk */}
         <Link to="/">
           <img src={logo} alt="store" className="navbar-brand" />
+        </Link>
+        <ul className="navbar-nav align-items-center">
+          <li className="nav-item ml-5">
+            <Link to="/" className="nav-link">
+              Products
+            </Link>
+          </li>
+        </ul>
+        <Link to="/cart" className="ml-auto">
+          <button>
+            <i className="fas fa-cart-plus" />
+          </button>
         </Link>
       </nav>
     );
