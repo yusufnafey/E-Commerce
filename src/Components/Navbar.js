68b7574195799d7eb7113f96bb47d84fe44bf794
Yusuf Nafey <yusufnafey@yahoo.com>
2019-03-18 23:35:37 -0700

    put button container component in new file

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import logo from "../logo.svg";
 import styled from "styled-components";
+import { ButtonContainer } from "./Button";
 
 export default class Navbar extends Component {
   render() {
@@ -33,23 +34,3 @@ https://www.iconfinder.com/Makoto_msk */}
     );
   }
 }
-
-const ButtonContainer = styled.button`
-  text-transform: capitalize;
-  font-size: 1.4rem;
-  background: transparent;
-  border: 0.05rem solid var(--lightBlue);
-  color: var(--lightBlue);
-  border-radius: 0.5rem;
-  padding: 0.2rem 0.5rem;
-  cursor: pointer;
-  margin: 0.5 rem 0.2rem 0;
-  transition: all 0.5s ease-in-out;
-  &:hover {
-    background: var(--lightBlue);
-    color: var(--mainBlue);
-  }
-  &:focus {
-    outline: none;
-  }
-`;
