ddae2969702186a27110eb1cc8012a87998c8b70
Yusuf Nafey <yusufnafey@yahoo.com>
2019-03-18 22:57:48 -0700

    got a styled component working

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import logo from "../logo.svg";
+import styled from "styled-components";
 
 export default class Navbar extends Component {
   render() {
@@ -20,12 +21,19 @@ https://www.iconfinder.com/Makoto_msk */}
           </li>
         </ul>
         <Link to="/cart" className="ml-auto">
-          <button>
-            <i className="fas fa-cart-plus" />
-            My Cart
-          </button>
+          <ButtonContainer>
+            <span className="mr-2">
+              <i className="fas fa-cart-plus" />
+              my cart
+            </span>
+            {/* SPAN ISN'T SPACING??? */}
+          </ButtonContainer>
         </Link>
       </nav>
     );
   }
 }
+
+const ButtonContainer = styled.button`
+  text-transform: capitalize;
+`;
