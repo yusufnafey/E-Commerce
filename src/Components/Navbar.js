62c29292d23859aee5b5660ec07f37695a5cea86
Yusuf Nafey <yusufnafey@yahoo.com>
2019-03-18 23:03:04 -0700

    set up a button styled component

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -22,10 +22,10 @@ https://www.iconfinder.com/Makoto_msk */}
         </ul>
         <Link to="/cart" className="ml-auto">
           <ButtonContainer>
-            <span className="mr-2">
-              <i className="fas fa-cart-plus" />
-              my cart
-            </span>
+            {/* <span className="mr-2"> */}
+            <i className="fas fa-cart-plus" />
+            my cart
+            {/* </span> */}
             {/* SPAN ISN'T SPACING??? */}
           </ButtonContainer>
         </Link>
@@ -36,4 +36,20 @@ https://www.iconfinder.com/Makoto_msk */}
 
 const ButtonContainer = styled.button`
   text-transform: capitalize;
+  font-size: 1.4rem;
+  background: transparent;
+  border: 0.05rem solid var(--lightBlue);
+  color: var(--lightBlue);
+  border-radius: 0.5rem;
+  padding: 0.2rem 0.5rem;
+  cursor: pointer;
+  margin: 0.5 rem 0.2rem 0;
+  transition: all 0.5s ease-in-out;
+  &:hover {
+    background: var(--lightBlue);
+    color: var(--mainBlue);
+  }
+  &:focus {
+    outline: none;
+  }
 `;
