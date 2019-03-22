3fa643a16ee678a91d5966a1c50db876ea483142
Yusuf Nafey <yusufnafey@yahoo.com>
2019-03-21 23:53:58 -0700

    created title component, set up structure for product list

--- src/Components/ProductList.js
+++ src/Components/ProductList.js
@@ -1,12 +1,21 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import Product from "./Product";
 
 export default class ProductList extends Component {
+  state = {
+    products: []
+  };
+
   render() {
     return (
-      <div>
-        <Product />
-      </div>
+      <Fragment>
+        <div className="py-5">
+          <div className="container">
+            <div className="row">hello</div>
+          </div>
+        </div>
+        {/* <Product /> */}
+      </Fragment>
     );
   }
 }
