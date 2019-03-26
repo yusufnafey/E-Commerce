96956432ae4799bbda1026b250d52d1e839517ce
Yusuf Nafey <yusufnafey@yahoo.com>
2019-03-25 23:16:17 -0700

    conditional statements for button disable and button icon

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -16,6 +16,19 @@ export default class Product extends Component {
             <Link to="/details">
               <img src={img} alt="product image" className="card-img-top" />
             </Link>
+            <button
+              className="card-btn"
+              disabled={inCart ? true : false}
+              onClick={() => console.log("added to cart")}
+            >
+              {inCart ? (
+                <p className="text-capitalize mb-0" disabled>
+                  In Cart
+                </p>
+              ) : (
+                <i className="fas fa-cart-plus" />
+              )}
+            </button>
           </div>
         </div>
       </ProductWrapper>
