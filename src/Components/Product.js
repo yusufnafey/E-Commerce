9231077c4a0d07a7b47201e5582fb06074678eac
Yusuf Nafey <yusufnafey@yahoo.com>
2019-03-25 23:18:19 -0700

    made data true, conditional renders accordingly

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -23,7 +23,7 @@ export default class Product extends Component {
             >
               {inCart ? (
                 <p className="text-capitalize mb-0" disabled>
-                  In Cart
+                  in cart
                 </p>
               ) : (
                 <i className="fas fa-cart-plus" />
