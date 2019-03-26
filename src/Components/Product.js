37e2869ec294b32a675297ba10680c8c73bf6af0
Yusuf Nafey <yusufnafey@yahoo.com>
2019-03-25 23:06:58 -0700

    images display on products page

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -7,9 +7,20 @@ export default class Product extends Component {
   render() {
     const { id, title, img, price, inCart } = this.props.product;
     return (
-      <div>
-        <h3>Product</h3>
-      </div>
+      <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
+        <div className="card">
+          <div
+            className="img-container p-5"
+            onClick={console.log("clicked on image container")}
+          >
+            <Link to="/details">
+              <img src={img} alt="product image" className="card-img-top" />
+            </Link>
+          </div>
+        </div>
+      </ProductWrapper>
     );
   }
 }
+
+const ProductWrapper = styled.div``;
