14ba1ec8e650391f34e2769e5128a3b2fa137510
Yusuf Nafey <yusufnafey@yahoo.com>
2019-04-05 22:58:29 -0700

    removed tester

--- src/context.js
+++ src/context.js
@@ -18,23 +18,6 @@ class ProductProvider extends Component {
     console.log("hello from add to cart");
   };
 
-  tester = () => {
-    console.log("State products: ", this.state.products[0].inCart);
-    console.log("Data products: ", storeProducts[0].inCart);
-
-    const tempProducts = [...this.state.products];
-    tempProducts[0].inCart = true;
-    this.setState(
-      () => {
-        return { products: tempProducts };
-      },
-      () => {
-        console.log("State products: ", this.state.products[0].inCart);
-        console.log("Data products: ", storeProducts[0].inCart);
-      }
-    );
-  };
-
   render() {
     return (
       <ProductContext.Provider
@@ -44,7 +27,6 @@ class ProductProvider extends Component {
           addToCart: this.addToCart
         }}
       >
-        <button onClick={this.tester}>test me</button>
         {this.props.children}
       </ProductContext.Provider>
     );
