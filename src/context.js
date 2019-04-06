e9cbc487faf961a7d31eb8e6af4b10c0ee822116
Yusuf Nafey <yusufnafey@yahoo.com>
2019-04-05 23:01:00 -0700

    implemented setProducts

--- src/context.js
+++ src/context.js
@@ -6,8 +6,19 @@ const ProductContext = React.createContext();
 
 class ProductProvider extends Component {
   state = {
-    products: storeProducts,
+    products: [],
     detailProduct: detailProduct
+  };
+
+  setProducts = () => {
+    let products = [];
+    storeProducts.forEach(item => {
+      const singleItem = { ...item };
+      products = [...products, singleItem];
+    });
+    this.setState(() => {
+      return { products: products };
+    });
   };
 
   handleDetail = () => {
