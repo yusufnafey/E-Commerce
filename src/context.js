3cec013e63659128d35534abd8cce54874d07c1e
Yusuf Nafey <yusufnafey@yahoo.com>
2019-04-05 22:53:59 -0700

    able to add first item in cart and change boolean value

--- src/context.js
+++ src/context.js
@@ -18,6 +18,17 @@ class ProductProvider extends Component {
     console.log("hello from add to cart");
   };
 
+  tester = () => {
+    console.log("State products: ", this.state.products[0].inCart);
+    console.log("Data products: ", storeProducts[0].inCart);
+
+    const tempProducts = [...this.state.products];
+    tempProducts[0].inCart = true;
+    this.setState(() => {
+      return { products: tempProducts };
+    });
+  };
+
   render() {
     return (
       <ProductContext.Provider
