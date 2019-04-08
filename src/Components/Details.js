b4b1a9045fe1c4896e4612976f0fa2e807b51648
Yusuf Nafey <yusufnafey@yahoo.com>
2019-04-07 22:18:54 -0700

    detail Product values are logging with click

--- src/Components/Details.js
+++ src/Components/Details.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
+import { ProductConsumer } from "../context";
+import { Link } from "react-router-dom";
+import { ButtonContainer } from "./Button";
 
 export default class Details extends Component {
   render() {
     return (
-      <div>
-        <h3>Details</h3>
-      </div>
+      <ProductConsumer>
+        {value => {
+          console.log(value.detailProduct);
+        }}
+      </ProductConsumer>
     );
   }
 }
