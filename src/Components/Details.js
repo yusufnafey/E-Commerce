0516f5b55021d22f147538b2b13547bef5d04eb2
Yusuf Nafey <yusufnafey@yahoo.com>
2019-04-08 23:14:48 -0700

    added product information to details page

--- src/Components/Details.js
+++ src/Components/Details.js
@@ -25,13 +25,25 @@ export default class Details extends Component {
                   <h1>{title}</h1>
                 </div>
               </div>
-              {/* info */}
               <div className="row">
-                <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
-                  col 1
+                <div className="col-10 mx-auto col-md-6 my-3">
+                  <img src={img} alt="product" className="img-fluid" />
                 </div>
                 <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
-                  col 2
+                  <h2>Model: {title}</h2>
+                  <h4 className="text-title text-uppercase text-muted mt-3 mb-2">
+                    Made by: <span className="text-uppercase">{company}</span>
+                  </h4>
+                  <h4 className="text-blue">
+                    <strong>
+                      Price: <span>$</span>
+                      {price}
+                    </strong>
+                  </h4>
+                  <p className="text-capitalize font-weight-bold mt-3 mb-0">
+                    Product info:
+                  </p>
+                  <p className="text-muted lead">{info}</p>
                 </div>
               </div>
             </div>
