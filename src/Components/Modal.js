aef4c17fe1c30ed8095a96915bf9f840ec73c380
Yusuf Nafey <yusufnafey@yahoo.com>
2019-04-18 00:50:01 -0700

    got modal displaying in middle with styled component

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -42,4 +42,11 @@ const ModalContainer = styled.div`
   left: 0;
   right: 0;
   bottom: 0;
+  background: rgba(0, 0, 0, 0.3);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  #modal {
+    background: var(--mainWhite);
+  }
 `;
