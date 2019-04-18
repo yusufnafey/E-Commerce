a36295837c07eba9b586e2538339757e269e6b90
Yusuf Nafey <yusufnafey@yahoo.com>
2019-04-17 23:53:01 -0700

    modal imports

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,4 +1,8 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+import { ProductConsumer } from "../context";
+import { ButtonContainer } from "./Button";
+import { Link } from "react-router-dom";
 
 export default class Modal extends Component {
   render() {
