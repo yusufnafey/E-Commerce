71046bfb91fb2fe40beed8d801b842d55f15db82
Yusuf Nafey <yusufnafey@yahoo.com>
2019-05-09 23:55:56 -0700

    created component for when the cart is empty

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Title from "../Title";
 import CartColumns from "./CartColumns";
+import EmptyCart from "./EmptyCart";
 
 export default class Card extends Component {
   render() {
@@ -8,6 +9,7 @@ export default class Card extends Component {
       <section>
         <Title name="your" title="cart" />
         <CartColumns />
+        <EmptyCart />
       </section>
     );
   }
