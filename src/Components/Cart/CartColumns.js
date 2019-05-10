36445fa8d5241393d8982dc9c875abbf1f98f58b
Yusuf Nafey <yusufnafey@yahoo.com>
2019-05-09 23:51:56 -0700

    fixed column names

--- src/Components/Cart/CartColumns.js
+++ src/Components/Cart/CartColumns.js
@@ -8,19 +8,19 @@ export default function CartColumns() {
           <p className="text-uppercase">products</p>
         </div>
         <div className="col-10 mx-auto col-lg-2">
-          <p className="text-uppercase">products</p>
+          <p className="text-uppercase">product name</p>
         </div>
         <div className="col-10 mx-auto col-lg-2">
-          <p className="text-uppercase">products</p>
+          <p className="text-uppercase">price</p>
         </div>
         <div className="col-10 mx-auto col-lg-2">
-          <p className="text-uppercase">products</p>
+          <p className="text-uppercase">quantity</p>
         </div>
         <div className="col-10 mx-auto col-lg-2">
-          <p className="text-uppercase">products</p>
+          <p className="text-uppercase">remove</p>
         </div>
         <div className="col-10 mx-auto col-lg-2">
-          <p className="text-uppercase">products</p>
+          <p className="text-uppercase">total</p>
         </div>
       </div>
     </div>
