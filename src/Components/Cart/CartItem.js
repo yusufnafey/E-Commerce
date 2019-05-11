068296f1d890b2ebc24bf5e51e60ffe6ae7a772e
Yusuf Nafey <yusufnafey@yahoo.com>
2019-05-10 23:23:32 -0700

    brought in specific item details and values

--- src/Components/Cart/CartItem.js
+++ src/Components/Cart/CartItem.js
@@ -1,6 +1,9 @@
 import React from "react";
 
-export default function CartItem() {
+export default function CartItem({ item, value }) {
+  const { id, title, img, price, total, count } = item;
+  const { increment, decrement, removeItem } = value;
+
   return (
     <div>
       cart item
