17485003f47b5222902fe15bb6d2213230b92190
Yusuf Nafey <yusufnafey@yahoo.com>
2019-05-12 23:58:20 -0700

    set up row/columns

--- src/Components/Cart/CartTotals.js
+++ src/Components/Cart/CartTotals.js
@@ -1,5 +1,16 @@
 import React from "react";
 
-export default function CartTotals() {
-  return <div />;
+export default function CartTotals({ value }) {
+  const { cartSubtotal, cartTax, cartTotal, clearCart } = value;
+  return (
+    <React.Fragment>
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mt-2 ml-ms-5 ml-md-auto col-sm-8 text-capitalize text-right">
+            hello
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 }
