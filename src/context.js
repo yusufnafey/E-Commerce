c227decaadeb732cb136e75e697d6166038eb4d9
Yusuf Nafey <yusufnafey@yahoo.com>
2019-05-15 23:53:17 -0700

    updates state to the new cart and products

--- src/context.js
+++ src/context.js
@@ -101,6 +101,16 @@ class ProductProvider extends Component {
 
     const index = tempProducts.indexOf(this.getItem(id));
     let removedProduct = tempProducts[index];
+    removedProduct.inCart = false;
+    removedProduct.count = 0;
+    removedProduct.total = 0;
+
+    this.setState(() => {
+      return {
+        cart: [...tempCart],
+        products: [...tempProducts]
+      };
+    });
   };
 
   clearCart = () => {
