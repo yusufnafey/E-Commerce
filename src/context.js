0e7331868dde27649adcf652e21af3fbfaa61e0a
Yusuf Nafey <yusufnafey@yahoo.com>
2019-05-17 23:54:45 -0700

    decrementation

--- src/context.js
+++ src/context.js
@@ -107,7 +107,15 @@ class ProductProvider extends Component {
   };
 
   decrement = id => {
-    console.log("decrement method");
+    let tempCart = [...this.state.cart];
+
+    const selectedProduct = tempCart.find(item => item.id === id);
+
+    const index = tempCart.indexOf(selectedProduct);
+    const product = tempCart[index];
+
+    product.count = product.count - 1;
+    product.total = product.count * product.price;
   };
 
   removeItem = id => {
