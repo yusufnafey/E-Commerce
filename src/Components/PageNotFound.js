ff0f552ddabc394f9b3021349d16058eb9b0ce27
Yusuf Nafey <yusufnafey@yahoo.com>
2019-05-19 23:15:52 -0700

    created 404 error headers

--- src/Components/PageNotFound.js
+++ src/Components/PageNotFound.js
@@ -4,8 +4,14 @@ export default class PageNotFound extends Component {
   render() {
     console.log(this.props);
     return (
-      <div>
-        <h3>PageNotFound</h3>
+      <div className="container">
+        <div className="row">
+          <div className=" col-10 mx-auto text-center text-title text-uppercase pt-5">
+            <h1 className="display-3">404</h1>
+            <h1>error</h1>
+            <h1>page not found</h1>
+          </div>
+        </div>
       </div>
     );
   }
