1ff769f033ccb91ac32b05cbcb49d61a0a3077ed
Yusuf Nafey <yusufnafey@yahoo.com>
2019-07-29 19:42:15 -0700

    more detailed product name

--- src/Components/Cart/CartItem.js
+++ src/Components/Cart/CartItem.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function CartItem({ item, value }) {
-  const { id, title, img, price, total, count } = item;
+  const { id, title, console, img, price, total, count } = item;
   const { increment, decrement, removeItem } = value;
 
   return (
@@ -16,7 +16,7 @@ export default function CartItem({ item, value }) {
       </div>
       <div className="col-10 mx-auto col-lg-2">
         <span className="d-lg-none">product:</span>
-        {title}
+        {title} ({console})
       </div>
       <div className="col-10 mx-auto col-lg-2">
         <span className="d-lg-none">price:</span>${price}
