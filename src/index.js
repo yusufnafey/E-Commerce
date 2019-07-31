4af7383ad7a476a4d7fb7d8cf96cf99c7517eb68
Yusuf Nafey <yusufnafey@yahoo.com>
2019-07-30 21:31:26 -0700

    got rid of browser router alias

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { ProductProvider } from "./context";
 import "./index.scss";
 import App from "./App";
 
 ReactDOM.render(
   <ProductProvider>
-    <Router>
+    <BrowserRouter>
       <App />
-    </Router>
+    </BrowserRouter>
   </ProductProvider>,
   document.getElementById("root")
 );
